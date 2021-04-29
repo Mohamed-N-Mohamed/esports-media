3ab01fd1ece116b10454af8cbbf1047fa764357c
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2021-04-29 20:11:04 +0100

    files updated

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,22 @@
 
 //get ekko
 //`https://api.pandascore.co/lol/champions?token=${apiKey}&search[name]=ekko`
-
+https://api.pandascore.co/lol/tournaments/running
 //get lcs and eu
 
 //api key
+
+async function test(){
+  const res = await fetch('https://widgets.shadow.gg/api/v1/realtime/upcoming_fixtures?title=all&token=u8yvJcXTH8Sm3kg1n5C4pzJv&page=1')
+  const data = await res.json();
+  console.log(data)
+}
+
+
+
+
+  
+
 const apiKey = "Sd6r6q1oNMPtqCki-w2T2IlChM_HiKPo3HV7uSKOMHVDuTzG5lA";
 
 //search
@@ -17,6 +29,9 @@ async function search() {
 }
 
 
-search();
+
+
+// search();
+test();
 
 console.log("ITS WORKING");
